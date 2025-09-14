73d106f86a90d091573f1031b1747d26ae7a931e
PeterWYC1 <psa.siar@gmail.com>
2025-09-13 20:16:12 -0500

    app funcionando con modelos 2

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -4,8 +4,8 @@ import joblib
 
 # --- Rutas de los modelos ---
 MODEL_PATHS = {
-    "Modelo 1": "models/decision_tree_model.pkl",
-    "Modelo 2": "models/knn_model.pkl"
+    "Arbol de Decisión": "models/decision_tree_model.pkl",
+    "KNN": "models/knn_model.pkl"
 }
 
 # --- Cargar modelos en memoria ---
